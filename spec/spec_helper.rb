813d01a27cb455ba1574db5ef14f47e7c2896bc0
David Asabina <david@supr.nu>
2016-05-29 19:49:09 +0200

    spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 require 'rack/test'
+require 'mongoid-rspec'
 
 require File.expand_path('../../config/env', __FILE__)
+
+RSpec.configure do |config|
+  config.include Mongoid::Matchers, type: :model
+end
