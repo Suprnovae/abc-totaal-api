6ea88106c2594648880d26984c4f67c2c1a4fd1c
David Asabina <david@supr.nu>
2016-05-29 22:05:07 +0200

    Clean up and create indexes before test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,8 @@ require File.expand_path('../../config/env', __FILE__)
 RSpec.configure do |config|
   config.include Mongoid::Matchers, type: :model
 end
+
+[Basic::Models::Report, Basic::Models::User].each do |model|
+  model.remove_indexes
+  model.create_indexes
+end
