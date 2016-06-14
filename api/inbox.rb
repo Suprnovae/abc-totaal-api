6d797676803714cdf2e91f1bd1e90267d40f1aa8
David Asabina <david@supr.nu>
2016-06-14 18:00:56 +0200

    Messy but it's a test for staging

--- api/inbox.rb
+++ api/inbox.rb
@@ -9,37 +9,56 @@ module Basic
        sender = params[:sender]
        from = params[:from]
        subject = params[:subject]
+       comment = params[:'body-plain']
+       attachments = (params[:'attachment-count'].to_i || 0)
        p "Recipient is #{recipient}"
        p "Sender is #{sender}"
        p "From #{from}"
        p "Subject is #{subject}"
+       p "Comment is #{comment}"
        #p "params #{params}"
        #p "request #{request}"
        #p request
-       p "There are #{params[:'attachment-count'] || 1} attachments"
+       p "Attachments #{attachments}"
        # how to get an attachment from Mailgun?
-       if params[:'attachment-count']
-         (1..params[:'attachment-count'].to_i).each do |attachment_id|
+       if attachments > 0
+         (1..attachments).each do |attachment_id|
            file = params["attachment-#{attachment_id}"]
-           p "FILE #{file}"
-           p "name #{file[:filename]}"
+           label = file[:filename].strip.downcase.splice!('.csv')
+           doc = Basic::Models::Report.find_or_initialize_by shortname: label
+           if false
+             doc.organization = subject
+             doc.comment = comment
+           end
+           p "label is #{label}"
+           #p "FILE #{file}"
+           #p "name #{file[:filename].downcase.splice!('.csv')}"
            p "file #{file[:tempfile]}"
            p "size #{file[:tempfile].size}"
            p "path #{file[:tempfile].path}"
-             data = CSV.read(file[:tempfile])
-             p "data is #{data}"
-           #p "head#{file[:head]}"
+           data = CSV.new(file[:tempfile], headers: true, converters: :all)
+           p "data is #{data}"
+           data.map do |row| 
+             {
+               description: row["KSF"],
+               predicted: row["Prognose"],
+               actual: row["Realisatie"],
+               tablets: [
+                 { text: row["Tot en met"]},
+                 { text: row["Jaar"]}
+               ]
+             }
+           end
+           p "data for #{label} is #{data}"
+           doc.data = data
+           if doc.save
+             status :created
+           else
+             status :conflict
+           end
          end
-       end
-#       if params[:attachments]
-#         attachments = JSON.parse(params[:attachment])
-#         attachments.each do |attachment, index|
-#           p " - Attachment is #{attachment}"
-#           p "   Contains #{params[:"attachment-#{index}"]}"
-#         end
-#       end
-       status :created
-       # status :conflict
+       end # if attachments exist
+       status 406 # missing information
     end
   end
 end
