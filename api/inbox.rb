ff820743757d2d8d0f7f3e2a10d12680f091b0bb
David Asabina <david@supr.nu>
2016-06-15 08:09:41 +0200

    Better labelling

--- api/inbox.rb
+++ api/inbox.rb
@@ -16,26 +16,24 @@ module Basic
        p "From #{from}"
        p "Subject is #{subject}"
        p "Comment is #{comment}"
-       #p "params #{params}"
-       #p "request #{request}"
-       #p request
        p "Attachments #{attachments}"
-       # how to get an attachment from Mailgun?
+
        if attachments > 0
          (1..attachments).each do |attachment_id|
            file = params["attachment-#{attachment_id}"]
-           label = file[:filename].strip.downcase.slice!('.csv')
+           filename = file[:filename]
+           basename = File.basename(filename, File.extname(filename))
+           label = filename..gsub!(/( )+/, '_').downcase
            doc = Basic::Models::Report.find_or_initialize_by shortname: label
-           if false
+           if attachments == 1
              doc.organization = subject
              doc.comment = comment
            end
-           p "label is #{label}"
-           #p "FILE #{file}"
-           #p "name #{file[:filename].downcase.splice!('.csv')}"
-           p "file #{file[:tempfile]}"
-           p "size #{file[:tempfile].size}"
-           p "path #{file[:tempfile].path}"
+           p "======="
+           p "Filename #{filename}"
+           p "Label #{label}"
+           p "Size #{file[:tempfile].size}"
+           p "Path #{file[:tempfile].path}"
            csv = CSV.new(file[:tempfile], headers: true, converters: :all)
            data = csv.to_a.map do |row| 
              {
@@ -48,7 +46,7 @@ module Basic
                ]
              }
            end
-           p "data for #{label} is #{data}"
+           p "Data for #{label} is #{data}"
            doc.data = data
            if doc.save
              status :created
