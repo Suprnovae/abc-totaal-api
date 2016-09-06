ebace3fe3e90d237a67c81f8d0ac11cd8de4c10f
David Asabina <david@supr.nu>
2016-09-06 15:58:16 +0200

    validate token value and protect uniqueness

--- models/token.rb
+++ models/token.rb
@@ -8,12 +8,15 @@ module Basic
 
       belongs_to :tokenizable, polymorphic: true
 
+      field :value, type: String
+      field :expires_on, type: Time, default: Time.now+600
+
+      index({ value: 1 }, { unique: true })
       index({ expires_on: 1 }, { expire_after_seconds: 0 })
 
-      field :value, type: String
-      field :expires_on, type: Date
-
       store_in collection: 'tokens'
+
+      validates_presence_of :value
     end
   end
 end
