99ed5c2af374a55d0c48c24637b1905b35af28c1
David Asabina <david@supr.nu>
2016-09-25 12:08:27 +0200

    spec report update hook

--- spec/api/inbox_spec.rb
+++ spec/api/inbox_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'json'
 
 describe Basic::API do
   include Rack::Test::Methods
@@ -7,16 +8,114 @@ describe Basic::API do
     Basic::API
   end
 
-  context 'POST basic email' do
-    it 'suceeds' do
-      params =  {
+  before(:each) {
+    Basic::Models::Report.destroy_all
+  }
+
+  context 'POST e-mail hook without attachment' do
+    let(:params) {
+      {
         'recipient': 'machine@example.com',
         'sender': 'jane@doe.tld',
         'from': 'jane@doe.tld',
         'subject': 'Test mail',
         'body-plain': 'Do you get this now?',
       }
+    }
+
+    it 'fails on account of an attachment being required' do
       post '/inbox', params
+      expect(JSON.parse(last_response.body)["error"]).to include(/[a|A]ttachment required/)
+    end
+  end
+
+  context 'POST e-mail hook with invalid attachments' do
+    let(:params) {
+      params = {
+        'recipient': 'api@example.com',
+        'sender': 'wouter@example.com',
+        'from': 'Wouter <wouter@example.com>',
+        'subject': 'ACME Corp',
+        'body-plain': 'Nieuwe cijfers',
+        'attachment-count': 3,
+        'attachment-1': Rack::Test::UploadedFile.new('spec/fixtures/reports/starkindustries.csv'),
+        'attachment-2': Rack::Test::UploadedFile.new('spec/fixtures/reports/Gekko & Co.csv'),
+        'attachment-3': Rack::Test::UploadedFile.new('spec/fixtures/reports/dummycsv'),
+        'attachment-4': Rack::Test::UploadedFile.new('spec/fixtures/reports/dummycsv'),
+      }
+    }
+
+    it 'fails on account of an attachment being required' do
+      expect { post '/inbox', params }.to change{ Basic::Models::Report.count }.by(2)
+      expect(last_response).to be_created
+    end
+  end
+
+  context 'POST email with single attachment' do
+    let(:params) {
+      params = {
+        'recipient': 'api@example.com',
+        'sender': 'wouter@example.com',
+        'from': 'Wouter <wouter@example.com>',
+        'subject': 'ACME Corp',
+        'body-plain': 'Nieuwe cijfers',
+        'attachment-count': 1,
+        'attachment-1': Rack::Test::UploadedFile.new('spec/fixtures/reports/acmeinc.csv')
+      }
+    }
+
+    it 'succeeds' do
+      post '/inbox', params
+      expect(last_response).to be_created
+
+      post '/inbox', params
+      expect(last_response).to be_ok
+    end
+  end
+
+  context 'POST email with multiple attachments' do
+    let(:params) {
+      params = {
+        'recipient': 'api@example.com',
+        'sender': 'wouter@example.com',
+        'from': 'Wouter <wouter@example.com>',
+        'subject': 'ACME Corp',
+        'body-plain': 'Nieuwe cijfers',
+        'attachment-count': 2,
+        'attachment-1': Rack::Test::UploadedFile.new('spec/fixtures/reports/wonka.csv'),
+        'attachment-2': Rack::Test::UploadedFile.new('spec/fixtures/reports/duff.csv'),
+      }
+    }
+
+    it 'succeeds' do
+      expect { post '/inbox', params }.to change{ Basic::Models::Report.count }.by(2)
+      expect(last_response).to be_created
+
+      expect { post '/inbox', params }.to change{ Basic::Models::Report.count }.by(0)
+      expect(last_response).to be_ok
+    end
+  end
+
+  context 'POST email with multiple attachments' do
+    let(:params) {
+      params = {
+        'recipient': 'api@example.com',
+        'sender': 'wouter@example.com',
+        'from': 'Wouter <wouter@example.com>',
+        'subject': 'ACME Corp',
+        'body-plain': 'Nieuwe cijfers',
+        'attachment-count': 2,
+        'attachment-1': Rack::Test::UploadedFile.new('spec/fixtures/reports/wonka.csv'),
+        'attachment-2': Rack::Test::UploadedFile.new('spec/fixtures/reports/duff.csv'),
+      }
+    }
+
+    it 'succeeds' do
+      expect { post '/inbox', params }.to change{ Basic::Models::Report.count }.by(2)
+      expect(last_response).to be_created
+
+      expect { post '/inbox', params }.to change{ Basic::Models::Report.count }.by(0)
+      expect(last_response).to be_ok
     end
   end
 end
